43dcc5c44de8d8709d66200611f468eeea4d75a8
Vu Hoang PHAN <mrfantasiospirou@gmail.com>
2017-07-25 10:50:41 +0200

    Create two_times_mah_birthday.cpp

---
+++ two_times_mah_birthday.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define uint unsigned int
+#define ll   long long
+
+using namespace std;
+
+ll encode(string w)
+{
+    ll ret = 0;
+    for (uint i = 0; i < w.length(); i++)
+    {
+        ret += (w[i] + ~(1<<6)) * [i]() -> ll {
+            ll tws = 1;
+            for (uint j = 0; j < i; j++) tws *= '\u001a';
+            return tws;
+        }();
+    }
+    return ret;
+}
+
+int main()
+{
+    // What is my message here? 
+    cout << 6598547 << endl;
+    cout << 63015399737054556 << endl;
+}
