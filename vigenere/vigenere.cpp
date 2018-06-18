a043315d2e92ebb4d366e358bebacbdaff83223f
Vu Hoang PHAN <vuhoangphan1307@gmail.com>
2018-06-18 23:25:35 +0200

    Use recursion

--- vigenere/vigenere.cpp
+++ vigenere/vigenere.cpp
@@ -2,19 +2,22 @@
 
 #define ui unsigned int
 #define s  string
-#define l  length
+#define l  length()
 
 using namespace std;
 
-s encode(s p, s k)
+s encode(s p, s k, ui i=0)
 {
-    return [&p,k]()->s{for(ui i=0;p[i];i++)p[i]=(p[i]+k[i%k.l()]-'@')%'_'+' ';return p;}();
+    if(!p[i])return p;
+    p[i]=(p[i]+k[i%k.l]-'@')%'_'+' ';
+    return encode(p,k,-~i);
 }
 
-s decode(s p, s k)
+s decode(s p, s k, ui i=0)
 {
-    return [&p,k]()->s{for(ui i=0;p[i];i++)p[i]=(p[i]-k[i%k.l()]+(p[i]>k[i%k.l()]?false:'_'))%'_'+' ';
-    return p;}();
+    if(!p[i])return p;
+    p[i]=(p[i]-k[i%k.l]+(p[i]>k[i%k.l]?false:'_'))%'_'+' ';
+    return decode(p,k,-~i);
 }
 
 int main()
@@ -22,6 +25,7 @@ int main()
     string p = "Well hello there little coder ~~~!!!";
     string k = "A_Key";
     string e = encode(p, k);
+    
     cout << e << endl;
     cout << decode(e, k) << endl;
 }
